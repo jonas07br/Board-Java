35167f4cbcbb84374e6cb65bd570bb3f6c2ad6ad
jonas07br <jonasrafa07@gmail.com>
2025-08-07 23:32:04 -0300

    fix: ajusta inicializacao da aplicacao

--- src/main/java/com/dio/desafio/Main.java
+++ src/main/java/com/dio/desafio/Main.java
@@ -1,15 +1,19 @@
 package com.dio.desafio;
 
 import com.dio.desafio.persistence.MigrationStrategy;
+import com.dio.desafio.ui.MainMenu;
 
 import static com.dio.desafio.persistence.config.ConnectionConfig.getConnection;
 
 public class Main {
     public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
         try(var connection = getConnection()){
             new MigrationStrategy(connection).executeMigration();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            menu.execute();
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("FALHA AO INICIAR APLICACAO");
         }
     }
 }
