66d38dc913c91ec72260fabcf216d18020c48ad8
jonas07br <jonasrafa07@gmail.com>
2025-08-07 21:38:00 -0300

    feat: adiciona chamada da migration na Main

--- src/main/java/com/dio/desafio/Main.java
+++ src/main/java/com/dio/desafio/Main.java
@@ -1,7 +1,15 @@
 package com.dio.desafio;
 
+import com.dio.desafio.persistence.MigrationStrategy;
+
+import static com.dio.desafio.persistence.config.ConnectionConfig.getConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try(var connection = getConnection()){
+            new MigrationStrategy(connection).executeMigration();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
